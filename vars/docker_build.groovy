de3826b968c112fedd3afbe0fef8f96fe2e2e817
Honey Pratap <144008101+iam-pratap@users.noreply.github.com>
2024-10-15 14:28:50 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,4 +1,4 @@
 // Define function
-def call(String ProjectName, String ImageTag, String DockerHubUser){
-  sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
+def call(String ProjectName, String ImageTag, String dockerHubUser){
+  sh "docker build -t ${dockerHubUser}/${ProjectName}:${ImageTag} ."
 }
